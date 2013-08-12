c3ae77da0e01201372369d04cfadc3d10dab0eb2
gparlakov <gparlakov@gmail.com>
2013-08-12 08:44:15 -0700

    Support modifications of exam and json-xml converter

--- JsonToXMLParser_CodeLibrary/JsonXMLConverter/Parser.cs
+++ JsonToXMLParser_CodeLibrary/JsonXMLConverter/Parser.cs
@@ -1,23 +1,29 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml;
-using System.Xml.Linq;
-
-
-namespace ConsoleApplication1
+﻿namespace Converter
 {
+    //downloaded through NuGet - Json.Net
+    using Newtonsoft.Json;
+
+    //From .Net Framework
+    using System.Xml;
+
     public static class Parser
     {
+        /// <summary>
+        /// This is the json-to-xml converter
+        /// </summary>
+        /// <param name="json">Json formatted string</param>
+        /// <returns>Xml document</returns>
         public static XmlDocument ParseJsonToXml(string json) 
         {
             var xmlDoc = JsonConvert.DeserializeXmlNode(json);
             return xmlDoc;
         }
 
+        /// <summary>
+        /// This is the xml-to-json converter
+        /// </summary>
+        /// <param name="xmlDoc">Xml document</param>
+        /// <returns>Json-formatted string</returns>
         public static string XmlToJson(XmlDocument xmlDoc)
         {            
             var serializedXml = JsonConvert.SerializeXmlNode(xmlDoc.DocumentElement);
