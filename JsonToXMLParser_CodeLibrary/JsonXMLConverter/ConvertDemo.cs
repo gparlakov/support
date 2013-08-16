66f48b7b9885b8e7b52f3d136f6115fedc5270c3
gparlakov <gparlakov@gmail.com>
2013-08-16 03:21:51 -0700

    Update demo and add project

---
+++ JsonToXMLParser_CodeLibrary/JsonXMLConverter/ConvertDemo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Converter
+{
+    class ConvertDemo
+    {
+        static void Main(string[] args)
+        {
+            //Takes an xml - this is the app.config file from the assembly
+            var xmlDoc = new System.Xml.XmlDocument();
+            xmlDoc.Load("../../App.config");
+
+            //Converts xml to json
+            var json = Parser.XmlToJson(xmlDoc);
+
+            //shows the json on console
+            Console.WriteLine(json);
+
+            //Converts json back to xml 
+            var newXmlDoc = Parser.ParseJsonToXml(json);            
+
+            //saves as new doc app_copy.xml
+            newXmlDoc.Save("../../App_Copy.xml");
+        }
+    }
+}
